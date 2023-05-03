f48698390c7b51a13a9500b6218ef9ab3862c9de
Chi-Jonathan <89598430+Chi-Jonathan@users.noreply.github.com>
2023-05-02 18:31:16 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import main
 import random
 
-# probability = float(input("\nPacket mark probability (0.2, 0.4, 0.5, 0.6, 0.8): "))
-# rate = int(input("Input x times more packets than the normal user the attacker should pump (x = 10, 100, 1000): "))
-# method = int(input("Input '0' for node sampling and '1' for edge sampling: "))
-# net = int(input("Input '3' for 3 branch network, '4' for 4 branch, and '5' for 5 branch: "))
-# num_attackers = int(input("Input the amount of attackers you want (should be less than number of branches): "))
-# print()
+probability = float(input("\nPacket mark probability (0.2, 0.4, 0.5, 0.6, 0.8): "))
+rate = int(input("Input x times more packets than the normal user the attacker should pump (x = 10, 100, 1000): "))
+method = int(input("Input '0' for node sampling and '1' for edge sampling: "))
+net = int(input("Input '3' for 3 branch network, '4' for 4 branch, and '5' for 5 branch: "))
+num_attackers = int(input("Input the amount of attackers you want (should be less than number of branches): "))
+print()
 
-probability = 0.4
-rate = 10
-method = 1
-net = 4
-num_attackers = 2
+# probability = 0.4
+# rate = 10
+# method = 1
+# net = 4
+# num_attackers = 2
 
 
 if net == 3:
